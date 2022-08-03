5904c1f59921450c7f9c657003cc0985b2342973
Uan Murphy <G00385338@gmit.ie>
2022-08-03 16:06:15 +0100

    changed file endings and fixed test structure, also added site-page component

---
+++ patron/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SitePageComponent } from './components/site-page/site-page.component';
+import './App.scss';
+
+export default function App() {
+  return (
+    <div className="App">
+      <SitePageComponent/>
+    </div>
+  );
+}
---
+++ patron/src/components/site-page/site-page.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './site-page.component.scss';
+
+export function SitePageComponent(){
+    return (
+        <div className='site-page'>
+            <div className="header">
+                <div className="logo"></div>
+                <div className="options"></div>
+            </div>
+        </div>
+    )
+}
