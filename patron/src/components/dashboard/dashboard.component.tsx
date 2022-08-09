716b2cd50a58ae808f98def52177f0261f9d5487
Uan Murphy <G00385338@gmit.ie>
2022-08-09 14:31:45 +0100

    updated dashboard component, installed firebase into project

--- patron/src/components/dashboard/dashboard.component.tsx
+++ patron/src/components/dashboard/dashboard.component.tsx
@@ -4,14 +4,6 @@ import './dashboard.component.scss';
 export function DashboardComponent(){
     return (
         <div className='site-page'>
-            <div className="header">
-                <div className="logo">
-                    <h1>easy-drinks</h1>
-                </div>
-                <div className="options">
-
-                </div>
-            </div>
             <div className='main-body'>
                 <div className='left-index'></div>
                 <div className='middle-panel'>
